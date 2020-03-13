7c67e57e9cc75dfd1612b343ed16bcee7e94104d
AliBenHalima <52865910+AliBenHalima@users.noreply.github.com>
2020-03-13 11:58:03 +0100

    Template Integration

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Main/header/header.component';
+import { MainComponent } from './Main/main/main.component';
+import { FooterComponent } from './Main/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
