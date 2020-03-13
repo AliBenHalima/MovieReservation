d1bc711a806e18ecda5dde9cbce10d15290c3f5c
AliBenHalima <52865910+AliBenHalima@users.noreply.github.com>
2020-03-13 19:46:50 +0100

    Templates sign in /up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
-import { NgModule } from '@angular/core';
+import { HeaderComponent } from './Main/header/header.component';
+import { NgModule,Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+
+ const routes: Routes = [
+  {path: "", component: HeaderComponent},
+  {path: "SignIn", component: SignInComponent},
+  {path: "SignUp", component: SignUpComponent},
+  {path: "NotFound", component: NotFoundComponent},
+  {path: "About", component: AboutComponent},
+  {path: "**", component: NotFoundComponent}
+ 
+ ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Main/header/header.component';
-import { MainComponent } from './Main/main/main.component';
-import { FooterComponent } from './Main/footer/footer.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
-    FooterComponent
+    SignInComponent,
+    SignUpComponent,
+    NotFoundComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
