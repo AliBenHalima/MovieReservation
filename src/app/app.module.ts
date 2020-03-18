fc3afa0e19f15ca3dca879ffb36938b4d78f7c09
Amasuo <oussamazay.1@gmail.com>
2020-03-18 15:59:26 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { Catalog1Component } from './catalog1/catalog1.component';
+import { FooterComponent } from './Main/footer/footer.component';
+import { HeaderComponent } from './Main/header/header.component';
 import { PaymentsComponent } from './admin-dashboard/payments/payments.component';
 import { UsersComponent } from './admin-dashboard/users/users.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
@@ -13,15 +16,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessagesComponent } from './admin-dashboard/messages/messages.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { CurrentUserComponent } from './admin-dashboard/users/current-user/current-user.component';
-=======
-import { Catalog1Component } from './catalog1/catalog1.component';
-import { TestHeaderComponent } from './test-header/test-header.component';
-import { HeaderComponent } from './Main/header/header.component';
-import { FooterComponent } from './Main/footer/footer.component';
->>>>>>> 49f698c90e1f6df225d48b25277edb609b4d1175
 
 @NgModule({
   declarations: [
@@ -35,14 +31,10 @@ import { FooterComponent } from './Main/footer/footer.component';
     MessagesComponent,
     UsersComponent,
     PaymentsComponent,
-<<<<<<< HEAD
-    CurrentUserComponent
-=======
-    Catalog1Component,
-    TestHeaderComponent,
+    CurrentUserComponent,
     HeaderComponent,
-    FooterComponent
->>>>>>> 49f698c90e1f6df225d48b25277edb609b4d1175
+    FooterComponent,
+    Catalog1Component,
   ],
   imports: [
     BrowserModule,
