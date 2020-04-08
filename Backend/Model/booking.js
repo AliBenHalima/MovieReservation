0a33628549a0f9f7e046ad314ef399a433c040da
DAOUDI <kaspermood@gmail.com>
2020-04-08 01:39:13 +0100

    ddd

--- Backend/Model/booking.js
+++ Backend/Model/booking.js
@@ -7,8 +7,8 @@ const bookingSchema = new mongoose.Schema({
     EndTime : {type:Date },
     salle : { type: String},
     Status : { type:String},
-    Id: {type:String},
-    movie : { type: mongoose.Schema.Types.ObjectId,ref:'movies' }
+    Id: {type:String}
+    // movie : { type: mongoose.Schema.Types.ObjectId,ref:'movies' }
     });
 
 module.exports = mongoose.model('booking',bookingSchema);
