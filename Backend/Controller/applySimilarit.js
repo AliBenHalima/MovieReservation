aa033252e74033d4cd5fac412905d15d76d23dad
AliBenHalima <52865910+AliBenHalima@users.noreply.github.com>
2020-05-08 21:19:03 +0100

    2nd similarity commita

--- Backend/Controller/applySimilarit.js
+++ Backend/Controller/applySimilarit.js
@@ -31,7 +31,7 @@ const router = express.Router();
 
 async function last(){
     var listRatings=[];
-    var finallist;
+    var users;
     var response = await fetch('http://localhost:3000/similarity/listComments');
     var listComments = await response.json();
     console.log(listComments);
@@ -72,14 +72,57 @@ async function last(){
             }
        }
     });
-   return finallist=listComments;
+    users=listComments;
+   
+   return {data:users};
     }
     please();
 console.log("here is final list ");
 
-    console.log(finallist);
+    console.log({data:users});
 }
 last();
+
+var users = {};
+
+for (var i = 0; i < users.length; i++) {
+  var name = users[i].name;
+ 
+  users[name] = users[i];
+}
+
+function euclideanSimilarity() {
+    var name1 = "SRO";
+    var name2 = "jihed";
+
+    var ratings1 = users[name1];
+    var ratings2 = users[name2];
+
+    var titles = Object.keys(ratings1);
+    var i = titles.indexOf('name');
+    titles.splice(i, 1);
+ 
+
+    var sumSquares = 0;
+    for (var i = 0; i < titles.length; i++) {
+      var title = titles[i];
+      var rating1 = ratings1[title];
+      var rating2 = ratings2[title];
+      if (rating1 != null && rating2 != null) {
+        var diff = rating1 - rating2;
+        sumSquares += diff * diff;
+      }
+    }
+    var d = sqrt(sumSquares);
+
+    var similarity = 1 / (1 + d);
+   console.log("Similarity is");
+   console.log(similarity);
+  }
+  euclideanSimilarity();
+
+
+
 // (async ()=>{
 //     last();
 //     var result= await please();
