6ba12158a4752ac3ec93543558f9607333f101c6
unknown <aissajihed75@gmail.com>
2020-05-19 02:15:56 +0100

    search

--- src/app/Main/main/main.component.ts
+++ src/app/Main/main/main.component.ts
@@ -6,31 +6,34 @@ import { AuthService } from '../../sign-up/auth.service';
 @Component({
 	selector: 'app-main',
 	templateUrl: './main.component.html',
-  styleUrls: [ './main.component.css' ],
-  providers: [MessageService]
+	styleUrls: [ './main.component.css' ],
+	providers: [ MessageService ]
 })
 export class MainComponent implements OnInit {
-
 	movies;
-	constructor(private apiService: UserApiService,private authService: AuthService,private messageService: MessageService) {}
+	constructor(
+		private apiService: UserApiService,
+		private authService: AuthService,
+		private messageService: MessageService
+	) {}
 
-  addSingle() {
-    this.messageService.add({severity:'success', summary:"Connected", detail: 'Hi , '+this.authService.get_userName()+' you\'re Welcome'});
-}
+	addSingle() {
+		this.messageService.add({
+			severity: 'success',
+			summary: 'Connected',
+			detail: 'Hi , ' + this.authService.get_userName() + " you're Welcome"
+		});
+	}
 
 	ngOnInit(): void {
-    console.log(this.authService.connected);
-   if (this.authService.connected)
-   {
-     console.log('enter');
+		console.log(this.authService.connected);
+		if (this.authService.connected) {
+			console.log('enter');
 
-     setTimeout (() => {
-      this.addSingle();
-   }, 700);
-
-
-   }
-
+			setTimeout(() => {
+				this.addSingle();
+			}, 700);
+		}
 
 		this.apiService.getMovies().subscribe((res: any) => {
 			console.log(res);
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -46,17 +46,18 @@ export class MoviesComponent implements OnInit {
 
 	upload(form: NgForm) {
 		this.hide();
-		this.movies.push(form.value);
 		const data = new FormData();
 		data.append('name', form.value.name);
 		data.append('cat', form.value.cat);
 		data.append('desc', form.value.desc);
-		data.append('file', this.image, this.image.name);
 		data.append('duration', form.value.duration);
 		data.append('prodName', form.value.prodName);
 		data.append('type', form.value.type);
-		this.UploadService.upload(data);
-		console.log('ok');
+		data.append('file', this.image, this.image.name);
+		this.UploadService.upload(data).subscribe((res) => {
+			form.value._id = res;
+			this.movies.push(form.value);
+		});
 	}
 
 	update(form: NgForm) {
@@ -70,11 +71,7 @@ export class MoviesComponent implements OnInit {
 		data.append('duration', form.value.duration);
 		data.append('prodName', form.value.prodName);
 		data.append('type', form.value.type);
-		if (this.image) {
-			data.append('file', this.image, this.image.name);
-		}
-		console.log(this.image.name);
-
+		if (this.image) data.append('file', this.image, this.image.name);
 		this.UploadService.updateMovie(data);
 	}
 }
