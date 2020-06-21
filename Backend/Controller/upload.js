da68ab455d38a670a07891b7d3e61a1f2c62b262
AliBenHalima <52865910+AliBenHalima@users.noreply.github.com>
2020-06-21 16:28:34 +0100

    final

--- Backend/Controller/upload.js
+++ Backend/Controller/upload.js
@@ -38,7 +38,72 @@ module.exports.deleteMovie = (req, res, next) => {
   );
 };
 
+// module.exports.updateMovie = (req, res, next) => {
+//   let url = req.protocol + "://" + req.get("host");
+//   console.log(req.file);
+//   Movie.findOne({
+//     _id: req.body.id
+//   }).then((movie) => {
+//     if (movie) {
+//       if (req.body.name) movie.name = req.body.name;
+//       if (req.body.cat) movie.category = req.body.cat;
+//       if (req.body.desc) movie.desc = req.body.desc;
+//       if (req.body.duration) movie.duration = req.body.duration;
+//       if (req.body.prodName) movie.prodName = req.body.prodName;
+//       if (req.body.type) movie.type = req.body.type;
+//       if (req.body.price) movie.price = req.body.price;
+//       if (req.body.trailer) movie.trailer = req.body.trailer;
+//       if (req.file && req.file.filename) {
+//         fs.unlink('../images' + movie.file, (err) => {
+//           if (err) {
+//             console.error(err);
+//           }
+//         });
+//         movie.file = url + "/images/" + req.file.filename;
+//       }
+//       movie.save();
+//       res.send(movie);
+//     }
+//   });
+// };
+
+// module.exports.upload = (req, res, next) => {
+//   let url = req.protocol + "://" + req.get("host");
+//   console.log(url);
+//   Movie.findOne({
+//     name: req.body.name
+//   }).then((movie) => {
+//     if (movie) {
+//       console.log('movie_exist');
+//     } else {
+//       const movie = new Movie({
+//         name: req.body.name,
+//         type: req.body.type,
+//         desc: req.body.desc,
+//         file: url + "/images/" + req.file.filename,
+//         duration: req.body.duration,
+//         price: req.body.price,
+//         trailer: req.body.trailer,
+//         rating: 0,
+//         prodName: req.body.prodName,
+//         category: req.body.cat,
+//         creator: req.userData.userId
+//       });
+
+//       movie.save((err, movie) => {
+//         if (movie) {
+//           //  console.log("ok");
+//           res.send({
+//             id: movie._id,
+//             image: movie.file
+//           });
+//         } else console.log(err);
+//       });
+//     }
+//   });
+// };
 module.exports.updateMovie = (req, res, next) => {
+  let url = req.protocol + "://" + req.get("host");
   console.log(req.file);
   Movie.findOne({
     _id: req.body.id
@@ -53,12 +118,12 @@ module.exports.updateMovie = (req, res, next) => {
       if (req.body.price) movie.price = req.body.price;
       if (req.body.trailer) movie.trailer = req.body.trailer;
       if (req.file && req.file.filename) {
-        fs.unlink('../src/assets/img/covers/' + movie.file, (err) => {
+        fs.unlink('../images' + movie.file, (err) => {
           if (err) {
             console.error(err);
           }
         });
-        movie.file = req.file.filename;
+        movie.file = url + "/images/" + req.file.filename;
       }
       movie.save();
       res.send(movie);
@@ -67,7 +132,7 @@ module.exports.updateMovie = (req, res, next) => {
 };
 
 module.exports.upload = (req, res, next) => {
-  url = req.protocol + "://" + req.get("host");
+  let url = req.protocol + "://" + req.get("host");
   console.log(url);
   Movie.findOne({
     name: req.body.name
